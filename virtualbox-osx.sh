8dfddf9a24f49af690636ab8afce3664a2dba65f
Evan Plaice <evanplaice@gmail.com>
2015-12-19 06:10:16 -0800

    Update `virtualbox-osx.sh` to encapsulate input strings
    Properly encapsulating all string inputs will prevent the script from
    breaking when an input contains spaces.

--- virtualbox-osx.sh
+++ virtualbox-osx.sh
@@ -2,37 +2,37 @@
 
 # instructions adapted from http://www.perkin.org.uk/posts/create-virtualbox-vm-from-the-command-line.html
 
-# store the profile name
+# set the profile name
 VM='OSX-El-Capitan'
 
 # create a new virtual disk
-VBoxManage createhd --filename $VM.vdi --variant fixed --size 32768
+VBoxManage createhd --filename "$VM_DIR/$VM.vdi" --variant fixed --size 32768
 
 # attach the virtual disk
-RAW_DISK=$(./vdi-attach.sh $VM.vdi | tail -n1)
+RAW_DISK=$(./vdi-attach.sh "$VM_DIR/$VM.vdi" | tail -n1)
 
 # format the virtual disk
-diskutil eraseDisk JHFS+ $VM $RAW_DISK
+diskutil eraseDisk JHFS+ "$VM" "$RAW_DISK"
 
 # detach the virtual disk
-hdiutil detach $RAW_DISK
+hdiutil detach "$RAW_DISK"
 
 # create an OSX 10.00 El Capitan x64 profile
-VBoxManage createvm --register --name $VM --ostype MacOS_64
+VBoxManage createvm --register --name "$VM" --ostype MacOS_64
 
 # create a new virtual SATA controller,  attach the virtual disk and installation iso
-VBoxManage storagectl $VM --name "SATA Controller" --add sata --controller IntelAHCI
-VBoxManage storageattach $VM --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium $VM.vdi
-VBoxManage storageattach $VM --storagectl "SATA Controller" --port 1 --device 0 --type dvddrive --medium ./ElCapitan.iso
+VBoxManage storagectl "$VM" --name "SATA Controller" --add sata --controller IntelAHCI
+VBoxManage storageattach "$VM" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$VM_DIR/$VM.vdi"
+VBoxManage storageattach "$VM" --storagectl "SATA Controller" --port 1 --device 0 --type dvddrive --medium ./ElCapitan.iso
 
 # modify some system settings
-VBoxManage modifyvm $VM --chipset piix3
-VBoxManage modifyvm $VM --firmware efi
-VBoxManage modifyvm $VM --memory 4096 --vram 128
-VBoxManage modifyvm $VM --mouse usbtablet --keyboard usb
+VBoxManage modifyvm "$VM" --chipset piix3
+VBoxManage modifyvm "$VM" --firmware efi
+VBoxManage modifyvm "$VM" --memory 4096 --vram 128
+VBoxManage modifyvm "$VM" --mouse usbtablet --keyboard usb
 
 # fix "Stuck on boot: "Missing Bluetooth Controller Transport"
-VBoxManage modifyvm $VM --cpuidset 00000001 000306a9 00020800 80000201 178bfbff
+VBoxManage modifyvm "$VM" --cpuidset 00000001 000306a9 00020800 80000201 178bfbff
 
 # start the VM
-VBoxManage startvm $VM --type gui
+VBoxManage startvm "$VM" --type gui
